f19f28e54df8d010138853dbd3cc09a5b83ecdaf
akshay <thesmileynoob@gmail.com>
2017-03-07 14:57:06 +0530

    Add missing timetable plugin

---
+++ blocks/timetable/locallib.php
@@ -0,0 +1,20 @@
+<?php
+
+function get_timetable(){
+    //  Initiate curl
+    $ch = curl_init();
+    // $url = 'http://192.168.1.161/moodle/timetable.php?id='.$_SESSION['USER']->username;
+    $url = 'http://192.168.1.161/moodle/timetable.php?id='.'817172';
+    // Disable SSL verification
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    // Will return the response, if false it print the response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // Set the url
+    curl_setopt($ch, CURLOPT_URL,$url);
+    // Execute
+    $result=curl_exec($ch);
+    // Closing
+    curl_close($ch);
+    return json_decode($result,true);
+}
+

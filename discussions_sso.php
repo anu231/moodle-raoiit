ece0b890ecc88814b0337685e8829b8415ee299f
Anurag <anu231@gmail.com>
2017-04-05 17:10:51 +0530

    rao manager updated

---
+++ discussions_sso.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('config.php');
+global $USER, $CFG;
+
+$secret = $CFG->secret_key;
+$sso = urldecode($_GET['sso']);
+$sig = urldecode($_GET['sig']);
+$sso_hash = hash_hmac('sha256',$sso,$secret);
+
+if ($sso_hash!=$sig){
+	echo 'Hash does not match';
+	exit;
+}
+$nonce = base64_decode($sso);
+$data = $nonce.'&name='.$USER['username'].'&username='.$USER['username'].'&email='.$USER['email'].'&external_id='.$USER['username'];
+//echo $data;
+$data_base64 = base64_encode($data);
+$sig_out = hash_hmac('sha256',$data_base64,$secret);
+$sso_out = urlencode($data_base64); 
+header("Location:".$CFG->discourse_url."sso_login?sso=".$sso_out.'&sig='.$sig_out);
+exit;
--- local/raomanager/lib.php
+++ local/raomanager/lib.php
@@ -12,29 +12,51 @@ function local_raomanager_pluginmap(){
         );
 }
 
+function user_has_access($username)
+{
+    $access_plugins = array();
+    global $DB;$record = $DB->get_record('raomanager_admins',array('username' => $username));
+    if (!$record){
+        return False;
+    } else {
+        foreach($record as $rec){
+            array_push($access_plugins,$rec['pluginname']);
+        }
+        return $access_plugins;
+    }
+} 
 
 function local_raomanager_extend_navigation(global_navigation $nav){
-
-    $previewnode = $nav->add('RaoManager', new moodle_url('/local/raomanager/index.php'), navigation_node::TYPE_CONTAINER);
-    $batchnode = $previewnode->add("Add/Edit Batch", new moodle_url('/local/raomanager/batch/index.php'));
-    $batchnode->make_active();
-    $centernode = $previewnode->add("Add/Edit Centers", new moodle_url('/local/raomanager/center/index.php'));
-    $centernode->make_active();
-    $notificationnode = $previewnode->add("Send Notifications", new moodle_url('/local/raomanager/notification/index.php'));
-    $notificationnode->make_active();
-    
-    if(is_siteadmin())
-        $raoadminnode = $previewnode->add("Manage Raoadmins", new moodle_url('/local/raomanager/admin/index.php'));
-        $raoadminnode->make_active();
-
+    global $USER;
+    $access_plugins = user_has_access($USER->username);
+    $isadmin = is_siteadmin();
+    if ($access_plugins == False && !$isadmin){
+        return;
+    } else {
+        $previewnode = $nav->add('RaoManager', new moodle_url('/local/raomanager/index.php'), navigation_node::TYPE_CONTAINER);
+        if ($isadmin || array_search(local_raomanager_pluginmap()[4],$access_plugins)){
+            $batchnode = $previewnode->add("Add/Edit Batch", new moodle_url('/local/raomanager/batch/index.php'));
+            $batchnode->make_active();
+            $centernode = $previewnode->add("Add/Edit Centers", new moodle_url('/local/raomanager/center/index.php'));
+            $centernode->make_active();
+        }
+        if ($isadmin || array_search(local_raomanager_pluginmap()[6],$access_plugins)){
+            $notificationnode = $previewnode->add("Send Notifications", new moodle_url('/local/raomanager/notification/index.php'));
+            $notificationnode->make_active();
+        }
+        if($isadmin) {
+            $raoadminnode = $previewnode->add("Manage Raoadmins", new moodle_url('/local/raomanager/admin/index.php'));
+            $raoadminnode->make_active();
+        }
+    }
 }
 
 // Check if the user has permission to use a plugin
 // return boolean. False for failure
 function local_raomanager_has_permission($pluginname) {
     global $USER, $DB;
-    // if( is_siteadmin() )
-    //     return TRUE;
+    if( is_siteadmin() )
+         return TRUE;
     $record = $DB->get_record('raomanager_admins', array('username' => $USER->username, 'pluginname' => $pluginname ));
     if(!$record)
         return FALSE;
@@ -44,4 +66,4 @@ function local_raomanager_has_permission($pluginname) {
         else
             return FALSE;
     }
-}   
+}  

1b8e049439e34749da786085ed54c3ef910aed73
Anurag <anu231@gmail.com>
2017-05-02 17:18:28 +0530

    discussions bug

--- discussions_sso.php
+++ discussions_sso.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 global $USER, $CFG;
 
-$secret = $CFG->discussions_secret_key;
+$secret = 'd836444a9e4084d5b224a60c208dce14';//$CFG->discussions_secret_key;
 $sso = urldecode($_GET['sso']);
 $sig = urldecode($_GET['sig']);
 $sso_hash = hash_hmac('sha256',$sso,$secret);
@@ -13,7 +13,7 @@ if ($sso_hash!=$sig){
 	exit;
 }
 $nonce = base64_decode($sso);
-$data = $nonce.'&name='.$USER['username'].'&username='.$USER['username'].'&email='.$USER['email'].'&external_id='.$USER['username'];
+$data = $nonce.'&name='.$USER->username.'&username='.$USER->username.'&email='.$USER->email.'&external_id='.$USER->username;
 //echo $data;
 $data_base64 = base64_encode($data);
 $sig_out = hash_hmac('sha256',$data_base64,$secret);

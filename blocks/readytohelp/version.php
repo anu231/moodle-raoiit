a58d7194cb3f4d92e3ce81afce62a62f0d2c67c8
Abhishek <abhishek.pawar@raoiit.com>
2017-06-05 13:08:24 +0530

    readytohelp version

--- blocks/readytohelp/version.php
+++ blocks/readytohelp/version.php
@@ -3,11 +3,18 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+//$plugin->component = 'block_readytohelp';
+//$plugin->version = 2017051000;
+//$plugin->requires = 2014051200;
+//$plugin->release = 'v0.2';
+//$plugin->cron = 0;
+//$plugin->dependencies = array();
+//$plugin->maturity = MATURITY_STABLE;
+
 $plugin->component = 'block_readytohelp';
-$plugin->version = 2017051700;
-$plugin->requires = 2014051200;
+$plugin->version = 2017052901;
 $plugin->release = 'v0.2';
+$plugin->requires = 2014051200;
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->cron = 0;
 $plugin->dependencies = array();
-$plugin->maturity = MATURITY_STABLE;
-

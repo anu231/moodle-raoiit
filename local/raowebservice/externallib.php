382ccd28cf0b7ce1e5f275341b10aba00c777fc0
Anurag <anu231@gmail.com>
2017-06-06 18:09:57 +0530

    profile updation

--- local/raowebservice/externallib.php
+++ local/raowebservice/externallib.php
@@ -45,31 +45,38 @@ class local_raowebservice_external extends external_api {
 ////////////////////////////////////////////////// User Profile Web Services ////////////////////////////////////////////////////////////////
 class local_raowebservice_profile extends external_api {
 
-public static function raouser_profile_update_parameters() {
+    public static function raouser_profile_update_parameters() {
         return new external_function_parameters(
-               array('username' => new external_value(PARAM_TEXT, 'User name whose info is to be modified', VALUE_REQUIRED),
-                     'field' => new external_value(PARAM_TEXT, 'Field name whose info is going to be modified', VALUE_REQUIRED),
-                     'value' => new external_value(PARAM_TEXT, 'New Value of the field', VALUE_REQUIRED),
-               )
+                array('username' => new external_value(PARAM_TEXT, 'User name whose info is to be modified', VALUE_REQUIRED),
+                        'field' => new external_value(PARAM_TEXT, 'Field name whose info is going to be modified', VALUE_REQUIRED),
+                        'value' => new external_value(PARAM_TEXT, 'New Value of the field', VALUE_REQUIRED),
+                )
         );
     }
 
-public static function raouser_profile_update($username, $field, $value) {
-    global $USER, $DB ,$CFG ;
+    public static function raouser_profile_update($username, $field, $value) {
+        global $USER, $DB ,$CFG ;
 
-    $params = self::validate_parameters(self::raouser_profile_update_parameters(), array('username' => $username,'field'=>$field,'value'=>$value));
+        //check if the user is site admin
+        /*if (!is_siteadmin($USER->id)){
+            //user is not admin deny webservice
+            return json_encode(array('status'=>'Failed','Info'=>'Only admin has access to this service'));
+        }*/
 
-    $user_data = $DB->get_record('user',array('username' => $username),'id',$strictness=IGNORE_MULTIPLE);
+        $params = self::validate_parameters(self::raouser_profile_update_parameters(), array('username' => $username,'field'=>$field,'value'=>$value));
 
-    $new_user = new stdClass();
-    $new_user->id= $user_data->id;
+        $user_data = $DB->get_record('user',array('username' => $username),'id',$strictness=IGNORE_MULTIPLE);
 
-    profile_load_data($new_user);
-    $new_user->$field = $value;
-    profile_save_data($new_user);
-    return json_encode((array)$new_user);
-}
-public static function raouser_profile_update_returns() {
-          return new external_value(PARAM_RAW, 'userdata');
+        $new_user = new stdClass();
+        $new_user->id= $user_data->id;
+
+        profile_load_data($new_user);
+        $new_user->$field = $value;
+        profile_save_data($new_user);
+        return json_encode((array)$new_user);
+    }
+
+    public static function raouser_profile_update_returns() {
+        return new external_value(PARAM_RAW, 'userdata');
     }
 }

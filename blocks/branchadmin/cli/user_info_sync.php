892743b5e07e992e4273c7da025e487ce37c9d34
Anurag <anu231@gmail.com>
2017-06-30 10:34:34 +0530

    individual username update supported

--- blocks/branchadmin/cli/user_info_sync.php
+++ blocks/branchadmin/cli/user_info_sync.php
@@ -1,11 +1,52 @@
 <?php
-
-define('CLI_SCRIPT',true);
+if ($_SERVER['REMOTE_ADDR'] != '203.123.46.194' && $_SERVER['REMOTE_ADDR'] != '192.168.1.19'){
+    define('CLI_SCRIPT',true);
+}
+echo constant('CLI_SCRIPT');
+//exit;
 require(__DIR__.'/../../../config.php');
 require_once($CFG->libdir.'/clilib.php');
 require_once($CFG->libdir.'/moodlelib.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
+//require_once('../locallib.php');
 
+function get_user_data_analysis($username){
+    //gets the users data from the analysis server
+    global $CFG;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $CFG->analysis_user_info_url.'?userid='.$username); //set the url
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); //return as a variable
+    $response = curl_exec($ch); //run the whole process and return the response
+    curl_close($ch); //close the curl handle
+    if( $response )
+        return json_decode($response, true);
+    else{
+        echo "Error getting data from :".$url;
+        return array();
+    }
+}
+
+function get_moodle_id($username){
+    global $DB;
+    $user_entry = $DB->get_record('user',array('username'=>$username));
+    return $user_entry->id;
+}
+
+function sync_user_data_analysis($username){
+    $user_data = get_user_data_analysis($username);
+    $user_profile = new stdClass();
+    $user_profile->id = get_moodle_id($username);
+    //cli_write($user->username.'-starting\n');
+    profile_load_data($user_profile);
+    $user_profile->profile_field_batch = $user_data['ttbatchid'];
+    $user_profile->profile_field_center = $user_data['centre'];
+    $user_profile->profile_field_fathername = $user_data['fathername'];
+    $user_profile->profile_field_birthdate = $user_data['birthdate'];
+    $user_profile->profile_field_studentmobile = $user_data['mobilenumber'];
+    $user_profile->profile_field_fathermobile = $user_data['mobilefather'];
+    $user_profile->profile_field_mothermobile = $user_data['mobilemother'];
+    profile_save_data($user_profile);
+}
 
 function get_user_data($userid){
     //gets the users data from the analysis server
@@ -25,10 +66,24 @@ function get_user_data($userid){
 
 global $DB;
 //get all the users aith auth == db 
-$user_list = $DB->get_records('user',array('auth'=>'db'));
+$user_list = array();
+if (!constant('CLI_SCRIPT')){
+    echo 'CLI_SCRIPT false';
+    if (isset($_GET['username'])) {
+        $t = new stdClass();
+        $t->username = $_GET['username'];
+        array_push($user_list,$t);
+    } else{
+        echo 'No username supplied';
+    }
+} else{
+    echo 'loading from db';
+    //$user_list = $DB->get_records('user',array('auth'=>'db'));
+}
 
 foreach($user_list as $user){
-    //get the info of this user 
+    //get the info of this user
+    /* 
     $user_data = get_user_data($user->username);
     $user_profile = new stdClass();
     $user_profile->id = $user->id;
@@ -41,6 +96,11 @@ foreach($user_list as $user){
     $user_profile->profile_field_studentmobile = $user_data['mobilenumber'];
     $user_profile->profile_field_fathermobile = $user_data['mobilefather'];
     $user_profile->profile_field_mothermobile = $user_data['mobilemother'];
-    profile_save_data($user_profile);
-    cli_write($user->username.'-updated\n');
+    profile_save_data($user_profile);*/
+    sync_user_data_analysis($user->username);
+    if (constant('CLI_SCRIPT')){
+        cli_write($user->username.'-updated\n');
+    } else{
+        echo $user->username.'-updated\n';
+    }
 }

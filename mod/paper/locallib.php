915039bef2e25a038d2c8e8372be61d1f5096f32
Anurag <anu231@gmail.com>
2017-07-31 15:36:33 +0530

    removed ssl verification from curl

--- mod/paper/locallib.php
+++ mod/paper/locallib.php
@@ -104,6 +104,7 @@ function paper_get_request($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url); //set the url
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); //return as a variable
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
     $response = curl_exec($ch); //run the whole process and return the response
     curl_close($ch); //close the curl handle
     if( $response )

441f61c518594559b26c6e42e4e2bffbc5fe12e9
Anurag <anu231@gmail.com>
2017-10-09 14:10:17 +0530

    form added for boolet

--- local/raotopiceditor/topic_form.php
+++ local/raotopiceditor/topic_form.php
@@ -3,12 +3,20 @@ defined('MOODLE_INTERNAL') || die();
 require_once("{$CFG->libdir}/formslib.php");
 
 require_once($CFG->dirroot.'/local/raotopiceditor/locallib.php');
+require_once($CFG->dirroot.'/mod/raobooklet/locallib.php');
 
 class raotopiceditor_booklet_form extends moodleform {
 
     function definition(){
         $mform =& $this->_form;
         $mform->addElement('hidden','topic',$this->_customdata['topic']);
-        $mform->addElement('text','name','');
+        $mform->setType('topic', PARAM_INT);
+        $mform->addElement('text','name','Name for the Booklet',array('size'=>'40'));
+        $mform->setType('name', PARAM_TEXT);
+        $booklets = list_booklets_select();
+        $mform->addElement('select', 'value','Select Booklet',$booklets);
+        $mform->setType('value', PARAM_INT);
+        //$mform->addElement('text','value',array('size'=>'40'));
+        $this->add_action_buttons($cancel=False, $submitlabel='Save Booklet Entry');
     }
 }
--- local/raotopiceditor/topicentries.php
+++ local/raotopiceditor/topicentries.php
@@ -2,6 +2,7 @@
 
 require_once('../../config.php');
 require_once('renderer.php');
+require_once('topic_form.php');
 
 global $PAGE, $CFG;
 require_login();
@@ -15,5 +16,23 @@ $output = $PAGE->get_renderer('local_raotopiceditor');
 $renderable = new topic_entries($topicid);
 
 echo $output->header();
+
+//adding new entries
+$form_class_names = array('raotopiceditor_booklet_form');
+$form_objects = Array();
+foreach($form_class_names as $form_name){
+    $form_objects[] = new $form_name(null, array('topic'=>$topicid));
+}
+
+foreach($form_objects as $fobj){
+    if ($form_data = $fobj->get_data()){
+        //form has received data in it
+        //process the data
+        //the display the form
+    }
+    $fobj->display();
+}
+
+
 echo $output->render($renderable);
 echo $output->footer();

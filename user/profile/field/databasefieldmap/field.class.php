b2fa01b6c478155ddff1a38ca47ed3a9b6ab4d18
Anurag <anu231@gmail.com>
2017-11-02 15:54:04 +0530

    for database fieldmap corrected for checkingthe id rather than analysis id

--- user/profile/field/databasefieldmap/field.class.php
+++ user/profile/field/databasefieldmap/field.class.php
@@ -64,7 +64,7 @@ class profile_field_databasefieldmap extends profile_field_base {
 
     function display_data() {
       global $DB;
-      $result_new=$DB->get_record($this->field->param1,array('analysis_id'=>$this->data));
+      $result_new=$DB->get_record($this->field->param1,array('id'=>$this->data));
       return $result_new->name;
     }
 

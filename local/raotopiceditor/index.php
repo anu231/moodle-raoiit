4478f3ef2273a32119238fc00a8dca4a6c06017b
Anurag <anu231@gmail.com>
2017-11-17 10:21:46 +0530

    allowed sahill access for topic editor

--- local/raotopiceditor/index.php
+++ local/raotopiceditor/index.php
@@ -3,12 +3,17 @@
 require_once('../../config.php');
 require_once('renderer.php');
 
-global $PAGE, $CFG;
 require_login();
+global $PAGE, $CFG, $USER;
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url('/local/raotopiceditor/index.php');
 $PAGE->set_pagelayout('standard');
 $PAGE->set_heading('Rao Topic Editor');
+
+if ($USER->username != 'admin' || $USER->username != 'sahiladmin'){
+    echo 'Access Denied';
+    exit;
+}
 
 $output = $PAGE->get_renderer('local_raotopiceditor');
 $renderable = new topics();
--- local/raotopiceditor/topicentries.php
+++ local/raotopiceditor/topicentries.php
@@ -4,13 +4,17 @@ require_once('../../config.php');
 require_once('renderer.php');
 require_once('topic_form.php');
 
-
-global $PAGE, $CFG;
 require_login();
+global $PAGE, $CFG, $USER;
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url('/local/raotopiceditor/topicentries.php');
 $PAGE->set_pagelayout('standard');
 $PAGE->set_heading('Rao Topic Editor');
+
+if ($USER->username != 'admin' || $USER->username != 'sahiladmin'){
+    echo 'Access Denied';
+    exit;
+}
 
 $topicid = required_param('topic',PARAM_INT);
 $action = optional_param('action',null, PARAM_TEXT);

433435a30a1069910630f482bde032fead079132
Anurag <anu231@gmail.com>
2018-01-16 14:16:48 +0530

    block settings set

--- blocks/library/block_library.php
+++ blocks/library/block_library.php
@@ -11,7 +11,8 @@ class block_library extends block_list {
         }
         $this->content = new stdClass;
         $this->content->items = array();
-        if (is_siteadmin()){
+        $course_active = get_config('library','manager_course');
+        if ($COURSE->shortname == $course_active){
             $books_add_url = new moodle_url('/blocks/library/add_books.php', array('blockid' => $this->instance->id,'courseid'=>$COURSE->id));
             $this->content->items[] = html_writer::link($books_add_url,"Add Books");
             $total_fine_url = new moodle_url('/blocks/library/view_total_fine.php', array('blockid' => $this->instance->id,'courseid'=>$COURSE->id));
@@ -43,5 +44,5 @@ class block_library extends block_list {
           return false;
     }
 
-    function has_config() {return false;}         
+    function has_config() {return true;}         
 }
--- blocks/library/settings.php
+++ blocks/library/settings.php
@@ -1,11 +1,26 @@
 <?php
-$day_setting = new admin_setting_text(
-            'library_issuedays',
+$day_setting = new admin_setting_configtext(
+            'library/issuedays',
             'Days to return the book in',
-            'Standard days the book should be returned in'
+            'Standard days the book should be returned in','7'
         );
-$day_setting->set_lockable(true);
+//$day_setting->set_lockable(true);
 $settings->add($day_setting);
+$fine_setting = new admin_setting_configtext(
+            'library/fine',
+            'Fine Per Day',
+            'Standard Fine amount','30'
+        );
+//$fine_setting->set_lockable(true);
+$settings->add($fine_setting);
+
+$course_setting = new admin_setting_configtext(
+            'library/manager_course',
+            'Course for admin access to library',
+            'Course for admin access to library','branchadministration'
+        );
+//$course_setting->set_lockable(true);
+$settings->add($course_setting);
 /* 
 $settings->add(new admin_setting_configcheckbox(
             'simplehtml/Allow_HTML',

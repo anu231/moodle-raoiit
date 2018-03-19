138ef1db1b174c3f8d36d7ae00d628b3cec1d740
Anurag <anu231@gmail.com>
2018-03-19 14:59:33 +0530

    ip check

--- auth/db/cli/sync_user.php
+++ auth/db/cli/sync_user.php
@@ -2,6 +2,32 @@
 
 require(__DIR__.'/../../../config.php');
 
+allowed_ips = array('203.123.46.194', '144.168.165.242', '104.227.244.29');
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+if (!array_key_exists(get_client_ip(), allowed_ips)){
+    echo 'ACCESS DENIED';
+    exit;
+}
+
 $username = required_param('username',PARAM_TEXT);
 
 $dbauth = get_auth_plugin('db');

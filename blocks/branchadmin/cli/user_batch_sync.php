7ff71f4c63c55d717363f3735b7202b1e889d5b3
Anurag <anu231@gmail.com>
2018-04-05 10:48:25 +0530

    user field update script modified;

--- blocks/branchadmin/cli/user_batch_sync.php
+++ blocks/branchadmin/cli/user_batch_sync.php
@@ -7,18 +7,22 @@ require_once($CFG->libdir.'/raolib.php');
 global $DB;
 
 $params = cli_get_params(array(
-    'batchfile' => false
+    'batchfile' => false,
+    'fields' => false
 ));
 
 $filename = $params[0]['batchfile'];
+$fields = explode(',',$params[0]['fields']);
 
 $cnt = 1;
 if (($handle = fopen($filename, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $username = trim($data[0]);
-        $ttbatch = trim($data[1]);
-        $extbatch = trim($data[2]);
-        echo "$cnt. $username|$ttbatch|$extbatch".PHP_EOL;
+        $field_data = array();
+        for ($i=0; $i<count($fields); $i++){
+            array_push($field_data, array('field'=>$fields[$i], 'val'=>trim($data[$i+1])));
+        }
+        echo "$cnt. $username | ".$field_data[0]['val'].PHP_EOL;
         if ($username == NULL || $username == ''){
             continue;
         }
@@ -26,11 +30,11 @@ if (($handle = fopen($filename, "r")) !== FALSE) {
         if (empty($user)){
             continue;
         }
-        if ($ttbatch != '' && $ttbatch != '0'){
-            set_profile_field('batch', $user->id, $ttbatch);
-        }
-        if ($extbatch != '' && $extbatch != '0'){
-            set_profile_field('extbatchid', $user->id, $extbatch);
+        for($i=0; $i< count($field_data); $i++){
+            $fd = $field_data[$i];
+            if ($fd['val'] != ''){
+                set_profile_field($fd['field'], $user->id, $fd['val']);
+            }
         }
         $cnt = $cnt+1;
     }

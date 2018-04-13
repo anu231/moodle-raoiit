067084999b73c1e465c8820449cc6e1fb8e69e58
Anurag <anu231@gmail.com>
2018-04-13 18:17:21 +0530

    minor error

--- blocks/branchadmin/locallib.php
+++ blocks/branchadmin/locallib.php
@@ -61,7 +61,7 @@ function get_batches_for_user(){
     global $DB, $USER;
     $sql = <<<EOT
     select tt.analysis_id, tt.name 
-    from (select id, name, centreid from {branchadmin_ttbatches} where status=1) as tt 
+    from (select id, analysis_id, name, centreid from {branchadmin_ttbatches} where status=1) as tt 
     join (select data as d, userid as uid from {user_info_data} as udata join {user_info_field} as uif on udata.fieldid=uif.id where uif.shortname='center') as ud 
     join {branchadmin_centre_info} as ci on tt.centreid=ci.analysis_id and ci.analysis_id=ud.d where ud.uid=?
 EOT;

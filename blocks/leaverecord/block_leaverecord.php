a5394d846600196fa7e22559265942dcf582c5e0
Abhishek <abhishek.pawar@raoiit.com>
2018-05-31 15:53:49 +0530

    apply leave function done by abhishek

--- blocks/leaverecord/block_leaverecord.php
+++ blocks/leaverecord/block_leaverecord.php
@@ -13,7 +13,7 @@ class block_leaverecord extends block_list {
         $this->content =  new stdClass;
         $this->content->items = array();
         $apply_leave_url = new moodle_url('/blocks/leaverecord/apply_leave.php', array('blockid' => $this->instance->id));
-        $view_leaves = new moodle_url('/blocks/leaverecord/view_leaves.php', array('blockid' => $this->instance->id));
+        $view_leaves = new moodle_url('/blocks/leaverecord/view_leave.php', array('blockid' => $this->instance->id));
         $this->content->items[0] = html_writer::link($apply_leave_url,"Apply for Leave");
         $this->content->items[1] = html_writer::link($view_leaves,"View Leaves");
 

0d070a3f97ed1302db5a2499a3161d5bb1abc7bd
Anurag <anu231@gmail.com>
2018-08-13 12:38:37 +0530

    committing this branch for further changes to rm_v3.4.2

--- comment/classes/privacy/provider.php
+++ comment/classes/privacy/provider.php
@@ -57,47 +57,13 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     /**
      * Writes user data to the writer for the user to download.
      *
-<<<<<<< HEAD
-     * @param  array  $context Contexts to run through and return data.
-=======
      * @param  \context $context The context to export data for.
->>>>>>> v3.5.1
      * @param  string $component The component that is calling this function
      * @param  string $commentarea The comment area related to the component
      * @param  int    $itemid An identifier for a group of comments
      * @param  array  $subcontext The sub-context in which to export this data
      * @param  bool   $onlyforthisuser  Only return the comments this user made.
      */
-<<<<<<< HEAD
-    public static function export_comments($context, $component, $commentarea, $itemid, $subcontext, $onlyforthisuser = true) {
-
-        $data = new \stdClass;
-        $data->context   = $context;
-        $data->area      = $commentarea;
-        $data->itemid    = $itemid;
-        $data->component = $component;
-
-        $commentobject = new \comment($data);
-        $commentobject->set_view_permission(true);
-        $comments = $commentobject->get_comments(0);
-        $subcontext[] = get_string('commentsubcontext', 'core_comment');
-
-        $comments = array_filter($comments, function($comment) use ($onlyforthisuser) {
-            global $USER;
-
-            return (!$onlyforthisuser || $comment->userid == $USER->id);
-        });
-
-        $comments = array_map(function($comment) {
-            return (object) [
-                'content' => $comment->content,
-                'time' => transform::datetime($comment->timecreated),
-                'userid' => transform::user($comment->userid),
-            ];
-        }, $comments);
-
-        if (!empty($comments)) {
-=======
     public static function export_comments(\context $context, string $component, string $commentarea, int $itemid,
                                            array $subcontext, bool $onlyforthisuser = true) {
         global $USER, $DB;
@@ -139,7 +105,6 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
 
         if (!empty($comments)) {
             $subcontext[] = get_string('commentsubcontext', 'core_comment');
->>>>>>> v3.5.1
             \core_privacy\local\request\writer::with_context($context)
                 ->export_data($subcontext, (object) [
                     'comments' => $comments,
@@ -148,15 +113,6 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
     }
 
     /**
-<<<<<<< HEAD
-     * Deletes all comments for a specified context.
-     *
-     * @param  \context $context Details about which context to delete comments for.
-     */
-    public static function delete_comments_for_all_users_in_context(\context $context) {
-        global $DB;
-        $DB->delete_records('comments', ['contextid' => $context->id]);
-=======
      * Deletes all comments for a specified context, component, and commentarea.
      *
      * @param  \context $context Details about which context to delete comments for.
@@ -198,7 +154,6 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         $DB->delete_records_select('comments',
             'contextid = :contextid AND component = :component AND commentarea = :commentarea AND itemid ' . $itemidstest,
             $params);
->>>>>>> v3.5.1
     }
 
     /**
@@ -206,28 +161,16 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
      *
      * @param  \core_privacy\local\request\approved_contextlist $contextlist Contains the user ID and a list of contexts to be
      * deleted from.
-<<<<<<< HEAD
-     */
-    public static function delete_comments_for_user(\core_privacy\local\request\approved_contextlist $contextlist) {
-=======
      * @param  string $component Component to delete from.
      * @param  string $commentarea Area to delete from.
      * @param  int $itemid The item id to delete from.
      */
     public static function delete_comments_for_user(\core_privacy\local\request\approved_contextlist $contextlist,
             string $component, string $commentarea = null, int $itemid = null) {
->>>>>>> v3.5.1
         global $DB;
 
         $userid = $contextlist->get_user()->id;
         $contextids = implode(',', $contextlist->get_contextids());
-<<<<<<< HEAD
-        $params = ['userid' => $userid];
-        list($insql, $inparams) = $DB->get_in_or_equal($contextlist->get_contextids(), SQL_PARAMS_NAMED);
-        $params += $inparams;
-
-        $select = "userid = :userid and contextid $insql";
-=======
         $params = [
             'userid' => $userid,
             'component' => $component,
@@ -246,7 +189,6 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         $params += $inparams;
 
         $select = "userid = :userid AND component = :component $areasql $itemsql AND contextid $insql";
->>>>>>> v3.5.1
         $DB->delete_records_select('comments', $select, $params);
     }
 }
